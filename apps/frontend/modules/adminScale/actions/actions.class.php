623f5ce5c18d029cec9a9c1c995f8992bfef7da8
bakyt <bakyt115@gmail.com>
2013-03-29 13:05:18 -0400

    admin scale

---
+++ apps/frontend/modules/adminScale/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * adminScale actions.
+ *
+ * @package    proj
+ * @subpackage adminScale
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class adminScaleActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
